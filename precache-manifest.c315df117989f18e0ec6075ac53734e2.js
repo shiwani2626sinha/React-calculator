48ad8bb2fd2082b008102125faf591da2721ba05
shiwani2626sinha <shiwani2626@gmail.com>
2020-07-08 18:18:31 +0530

    Updates

---
+++ precache-manifest.c315df117989f18e0ec6075ac53734e2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "077f982f07fbc5cc3bd67a47f01865ac",
+    "url": "/React-calculator/index.html"
+  },
+  {
+    "revision": "4104513a5e5af410b9f5",
+    "url": "/React-calculator/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "108fe3b7344a4bf335e9",
+    "url": "/React-calculator/static/css/main.195cb3cc.chunk.css"
+  },
+  {
+    "revision": "4104513a5e5af410b9f5",
+    "url": "/React-calculator/static/js/2.6d070467.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-calculator/static/js/2.6d070467.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "108fe3b7344a4bf335e9",
+    "url": "/React-calculator/static/js/main.9ba17801.chunk.js"
+  },
+  {
+    "revision": "725b801b6bfc064a7839",
+    "url": "/React-calculator/static/js/runtime-main.d172a17d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
